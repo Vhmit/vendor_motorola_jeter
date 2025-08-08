0db1466d602f4249739f66fb072699147e769580
Vhmit <viktor.9630@protonmail.com>
2025-08-08 09:19:41 -0300

    msm8937-common: Add support to moto g6 play
    Change-Id: I5dc9ceff06802d3b380f21ffbb209bc7d08c9242
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2021 The LineageOS Project
+# Copyright (C) 2019-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter ahannah cedric hannah james montana rhannah,$(TARGET_DEVICE)),)
+ifneq ($(filter ahannah cedric hannah james jeter montana rhannah,$(TARGET_DEVICE)),)
 
 endif
